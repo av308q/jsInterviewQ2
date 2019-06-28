9f03e7b46182ec47cad9396242c33d034c10f006
Angel Valdivia <angelv1901@gmail.com>
2019-06-28 14:31:12 -0400

    interviewQ

---
+++ moreinterviewQ.js
@@ -0,0 +1,28 @@
+// big O notation: making code more efficient, cutting down an array//
+//The DOM- document Object model - the html 
+// the event loop- a contantly running procces that checks if the call back is empty
+//closures have access to outer function variable even after the outer function is returned
+// MAP will count and return Array
+//ForEach will count but not return 
+// varEach will count but not return 
+// Var allows you to change variable 
+// let is blocked scoped 
+// const 
+// "this" Refrences a name or object 
+// name: john  - hello there "this.name"
+//
+
+
+
+// [1,2,3,4].filter(n => n < 3)
+
+function reversing (letters) {
+    letters.length -1 
+    for (let i=0; i < letters.length; i++){
+        console.log(letters[letters.length-1-i])
+    }
+
+}
+
+reversing("angel")
+
